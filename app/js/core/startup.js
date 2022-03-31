b52a31fca4ee9a7b3ad2f7d8ccd8defc90417eef
Tan Nguyen <tan.nguyenminh.cs07@gmail.com>
2022-03-31 01:08:29 -0500

    Add simple login page

--- app/js/core/startup.js
+++ app/js/core/startup.js
@@ -1,5 +1,10 @@
 
-$(function() {
+const userCredential = {
+	admin: 'bibleadmin',
+	user1: 'bibleuser1-123',
+	user2: 'bibleuser2-123'
+}
+function startup() {
 	// hide initial text area
 	$('#startup').hide();
 
@@ -83,4 +88,30 @@ $(function() {
 
 		$('.i18n').i18n();
 	}
+};
+
+function login() {
+	var username = $('#username').val();
+	var password = $('#password').val();
+
+	if (userCredential[username] == password){
+		$('#login').hide();
+		startup();
+	}
+	else {
+		$('#username').val("");
+		$('#password').val("");
+		$('#errorMsg').text("Username/Password incorrect, please try again.");
+		
+	}
+}
+
+function clearMsg() {
+	$('#errorMsg').text("");
+}
+
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        login();
+    }
 });
