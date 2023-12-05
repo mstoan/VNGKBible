ce76615f0d62092e2755b592bd23d2bd554a575b
msToan <msToan@gmail.com>
2023-12-05 08:17:00 -0600

    Update startup.js

--- app/js/core/startup.js
+++ app/js/core/startup.js
@@ -1,9 +1,10 @@
-// Login Information Here
+/* Login Information Here
 const userCredential = {
 	admin: 'bibleadmin',
 	user1: 'bibleuser1-123',
 	user2: 'bibleuser2-123'
 }
+*****/
 function startup() {
 	// hide initial text area
 	$('#startup').hide();
@@ -89,7 +90,7 @@ function startup() {
 		$('.i18n').i18n();
 	}
 };
-// LOGIN FUNCTION HERE
+/* LOGIN FUNCTION HERE
 function login() {
 	var username = $('#username').val();
 	var password = $('#password').val();
@@ -118,4 +119,5 @@ $(document).on('keypress',function(e) {
    if(e.which == 13) {
         login();
     }
+*/
 });
