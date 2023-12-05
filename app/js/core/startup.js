85a7adc4d6cd2b3288d2df0efcd58df07f462a96
msToan <msToan@gmail.com>
2023-12-05 10:26:54 -0600

    Try again

--- app/js/core/startup.js
+++ app/js/core/startup.js
@@ -1,10 +1,5 @@
-// Login Information Here
-const userCredential = {
-	admin: 'bibleadmin',
-	user1: 'bibleuser1-123',
-	user2: 'bibleuser2-123'
-}
-function startup() {
+// msT function startup() {
+$(function() {
 	// hide initial text area
 	$('#startup').hide();
 
@@ -88,33 +83,4 @@ function startup() {
 
 		$('.i18n').i18n();
 	}
-};
-
-function login() {
-	var username = $('#username').val();
-	var password = $('#password').val();
-
-	$('#login').hide(); //MST-Temporary bypass Login validation
-	startup(); //MST-Temporary bypass
-// Start
-	if (userCredential[username] == password){
-		$('#login').hide();
-		startup();
-	}
-	else {
-		$('#username').val("");
-		$('#password').val("");
-		$('#errorMsg').text("Username/Password incorrect, please try again.");
-	}
-// Stop
-}
-
-function clearMsg() {
-	$('#errorMsg').text("");
-}
-
-$(document).on('keypress',function(e) {
-    if(e.which == 13) {
-        login();
-    }
 });
