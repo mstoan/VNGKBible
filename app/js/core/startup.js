b1a2b610271aa0c76fb205cbff7442ff32ad63b7
msToan <msToan@gmail.com>
2023-12-05 07:31:38 -0600

    Remove Login Functions Properly
    Remove Login Functions in
    index.html and js/core/startup.js

--- app/js/core/startup.js
+++ app/js/core/startup.js
@@ -1,9 +1,11 @@
 // Login Information Here
+/* Mst-Remove Login for now 2023-12-05
 const userCredential = {
 	admin: 'bibleadmin',
 	user1: 'bibleuser1-123',
 	user2: 'bibleuser2-123'
 }
+*/
 function startup() {
 	// hide initial text area
 	$('#startup').hide();
@@ -89,7 +91,7 @@ function startup() {
 		$('.i18n').i18n();
 	}
 };
-
+/* msT-Remove Login function for now 2023-12-05
 function login() {
 	var username = $('#username').val();
 	var password = $('#password').val();
@@ -108,13 +110,16 @@ function login() {
 	}
 // Stop
 }
-
+*/
 function clearMsg() {
 	$('#errorMsg').text("");
 }
 
+/* msT-Remove Login function for now 2023-12-05
 $(document).on('keypress',function(e) {
-    if(e.which == 13) {
+  startup(); //MST-Temporary bypass
+   if(e.which == 13) {
         login();
     }
+*/
 });
