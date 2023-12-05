6c6216864ec62e475552aea45d11908180532b85
msToan <msToan@gmail.com>
2023-12-05 11:30:59 -0600

    1C

--- app/js/core/startup.js
+++ app/js/core/startup.js
@@ -1,7 +1,12 @@
-// msT function startup() {
-$(function() {
+// Login Information Here
+const userCredential = {
+	admin: 'bibleadmin',
+	user1: 'bibleuser1-123',
+	user2: 'bibleuser2-123'
+}
+function startup() {
 	// hide initial text area
-	$('#startup').hide();
+	/*$('#startup').hide();
 
 	// test for local file support
 	if (window.location.protocol === 'file:') {
@@ -46,7 +51,9 @@ $(function() {
 
 	} else {
 		init();
-	}
+	}*/
+
+	init();
 
 	function init() {
 		// load config
@@ -83,4 +90,34 @@ $(function() {
 
 		$('.i18n').i18n();
 	}
+};
+
+function login() {
+	var username = $('#username').val();
+	var password = $('#password').val();
+
+	$('#login').hide(); //MST-Temporary bypass Login validation
+	startup(); //MST-Temporary bypass
+// Start
+	if (userCredential[username] == password){
+		$('#login').hide();
+		startup();
+	}
+	else {
+		$('#username').val("");
+		$('#password').val("");
+		$('#errorMsg').text("Username/Password incorrect, please try again.");
+	}
+// Stop
+}
+
+function clearMsg() {
+	$('#errorMsg').text("");
+}
+
+$(document).on('keypress',function(e) {
+  startup(); //MST-Temporary bypass
+   if(e.which == 13) {
+        login();
+    }
 });
