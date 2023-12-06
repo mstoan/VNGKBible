5a1edf1a983ae316922cb9c56690acb03d96bb38
msToan <msToan@gmail.com>
2023-12-06 08:13:05 -0600

    No Login

--- app/js/core/startup-with-login.js
+++ app/js/core/startup-with-login.js
@@ -1,12 +1,11 @@
-
+// This file goes with index-with-login.html -->
 // Login Information Here
-// const userCredential = {
-// 	admin: 'bibleadmin',
-// 	user1: 'bibleuser1-123',
-// 	user2: 'bibleuser2-123'
-// }
-
-$(function startup() {
+const userCredential = {
+	admin: 'bibleadmin',
+	user1: 'bibleuser1-123',
+	user2: 'bibleuser2-123'
+}
+function startup() {
 	// hide initial text area
 	$('#startup').hide();
 
@@ -90,34 +89,33 @@ $(function startup() {
 
 		$('.i18n').i18n();
 	}
+};
+
+function login() {
+	var username = $('#username').val();
+	var password = $('#password').val();
+
+//	$('#login').hide(); //MST-Temporary bypass Login validation
+//	startup(); //MST-Temporary bypass
+// Start
+	if (userCredential[username] == password){
+		$('#login').hide();
+		startup();
+	}
+	else {
+		$('#username').val("");
+		$('#password').val("");
+		$('#errorMsg').text("Username/Password incorrect, please try again.");
+	}
+// Stop
+}
+
+function clearMsg() {
+	$('#errorMsg').text("");
+}
+
+$(document).on('keypress',function(e) {
+   if(e.which == 13) {
+        login();
+    }
 });
-
-// function login() {
-// 	var username = $('#username').val();
-// 	var password = $('#password').val();
-
-// 	$('#login').hide(); //MST-Temporary bypass Login validation
-// 	startup(); //MST-Temporary bypass
-// // Start
-// 	if (userCredential[username] == password){
-// 		$('#login').hide();
-// 		startup();
-// 	}
-// 	else {
-// 		$('#username').val("");
-// 		$('#password').val("");
-// 		$('#errorMsg').text("Username/Password incorrect, please try again.");
-// 	}
-// // Stop
-// }
-
-// function clearMsg() {
-// 	$('#errorMsg').text("");
-// }
-
-// $(document).on('keypress',function(e) {
-// //   startup(); //MST-Temporary bypass
-//    if(e.which == 13) {
-//         login();
-//     }
-// });
